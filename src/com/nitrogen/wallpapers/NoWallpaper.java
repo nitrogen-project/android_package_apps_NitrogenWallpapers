CyanogenMod/android_packages_apps_CMWallpapers

Add back no wallpaper option
Change-Id: I66469489973af60dbb601324e1bcc89d22b063f3

src/com/cyanogenmod/wallpapers/NoWallpaper.java
@@ -1,0 +1,33 @@
+/*		
+* Copyright (C) 2011 The Android Open Source Project		
+*		
+* Licensed under the Apache License, Version 2.0 (the "License");		
+* you may not use this file except in compliance with the License.		
+* You may obtain a copy of the License at		
+*		
+*      http://www.apache.org/licenses/LICENSE-2.0		
+*		
+* Unless required by applicable law or agreed to in writing, software		
+* distributed under the License is distributed on an "AS IS" BASIS,		
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		
+* See the License for the specific language governing permissions and		
+* limitations under the License.		
+*/
+
+package com.cyanogenmod.wallpapers;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.os.Bundle;
+
+public class NoWallpaper extends Activity {
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        finish();
+        try {
+            WallpaperManager wm = WallpaperManager.getInstance(this);
+            wm.setResource(R.drawable.black);
+        } catch (java.io.IOException e) {
+        }
+    }
+} 		

nitrogen-project/android_package_apps_NitrogenWallpapers

src/com/nitrogen/wallpapers/NoWallpaper.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitrogen.wallpapers;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.os.Bundle;
+
+public class NoWallpaper extends Activity {
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        finish();
+        try {
+            WallpaperManager wm = WallpaperManager.getInstance(this);
+            wm.setResource(R.drawable.black);
+        } catch (java.io.IOException e) {
+        }
+    }
+}