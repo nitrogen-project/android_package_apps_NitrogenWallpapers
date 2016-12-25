ed925858bb2a1eaf0e929dd70c8634ad405b6ccd
xyyx <xyyx@mail.ru>
2016-12-25 09:36:31 +0800

    Revert "CMWallpapers: Add a set wallpaper button for nowallpaper"
    This reverts commit 7641347ff5cdfc934fda57ad95691cab0ac6e959.

--- src/com/nitrogen/wallpapers/NoWallpaper.java
+++ src/com/nitrogen/wallpapers/NoWallpaper.java
@@ -19,22 +19,15 @@ package com.nitrogen.wallpapers;
 import android.app.Activity;
 import android.app.WallpaperManager;
 import android.os.Bundle;
-import android.view.View;
 
 public class NoWallpaper extends Activity {
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-
-        setContentView(R.layout.wallpaper_preview);
-    }
-
-    public void setWallpaper(View v) {
+        finish();
         try {
             WallpaperManager wm = WallpaperManager.getInstance(this);
             wm.setResource(R.drawable.black);
         } catch (java.io.IOException e) {
         }
-        setResult(RESULT_OK);
-        finish();
     }
 }
